af37c5bf86f5f3c9a72f00e5b7443c80cb3cac13
jack <jack@jack-VirtualBox>
2015-07-02 21:28:02 +0900

    modify

--- src/main/java/jackprivate/kttest/QpsTest.java
+++ src/main/java/jackprivate/kttest/QpsTest.java
@@ -10,31 +10,37 @@ import net.rubyeye.xmemcached.utils.AddrUtil;
 
 public class QpsTest {
 
-    public static void main(String[] args) throws IOException, InterruptedException, MemcachedException, TimeoutException {
-        int qps = Integer.valueOf(args[0]);
-        int second = Integer.valueOf(args[1]);
+    public static void main(String[] args) throws IOException {
+        String dataname = args[0];
+        int qps = Integer.valueOf(args[1]);
+        int second = Integer.valueOf(args[2]);
         XMemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses("localhost:2010"));
         XMemcachedClient client=(XMemcachedClient) builder.build();
         
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<20;i++){
-            sb.append("1234567890");
-        }
-        String message = sb.toString();
-        long startTime = System.currentTimeMillis();
-        for(long i=0;i<second;i++){
-            for(int j=0;j<qps;j++){
-                client.setWithNoReply("qpsdata"+i+"/"+j, 0, message+i+"/"+j);
+        try {
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<20;i++){
+                sb.append("1234567890");
             }
-            Thread.sleep(1000);
-        }
-        
-        long endTime = System.currentTimeMillis();
+            String message = sb.toString();
+            long startTime = System.currentTimeMillis();
+            for(long i=0;i<second;i++){
+                for(int j=0;j<qps;j++){
+                    client.setWithNoReply(dataname+"_"+i+"_"+j, 0, message+"_"+i+"_"+j);
+                }
+                Thread.sleep(1000);
+            }
+            
+            long endTime = System.currentTimeMillis();
  
-        
-        System.out.println("It tooks "+ ((endTime-startTime))+"ms.");
-        
-        client.shutdown();
+            
+            System.out.println("It tooks "+ ((endTime-startTime))+"ms.");
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            client.shutdown();
+        }
     }
 
 }
--- src/main/java/jackprivate/kttest/SimpleTest.java
+++ src/main/java/jackprivate/kttest/SimpleTest.java
@@ -10,30 +10,40 @@ import net.rubyeye.xmemcached.utils.AddrUtil;
 
 public class SimpleTest {
 
-    public static void main(String[] args) throws IOException, InterruptedException, MemcachedException, TimeoutException {
+    public static void main(String[] args) throws IOException  {
+        String dataname = args[0];
+        int count = Integer.valueOf(args[1]);
+        int offset = Integer.valueOf(args[2]);
         XMemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses("localhost:2010"));
         XMemcachedClient client=(XMemcachedClient) builder.build();
         
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<20;i++){
-            sb.append("1234567890");
-        }
-        String message = sb.toString();
-        long startTime = System.currentTimeMillis();
-        for(long i=0;i<100*1000*1000;i++){
-            client.setWithNoReply("basedata"+i, 0, message+i);
-            while(client.getConnector().getNoReplyOpsFlowControl().permits()<10000){
-                System.out.println("done="+ i +". available connection= "+ client.getConnector().getNoReplyOpsFlowControl().permits());
-                Thread.sleep(1000);
+        try{
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<20;i++){
+                sb.append("1234567890");
             }
+            String message = sb.toString();
+            long startTime = System.currentTimeMillis();
+            for(long i=0;i<100*1000*1000;i++){
+                client.setWithNoReply(dataname+i, 0, message+i);
+                while(client.getConnector().getNoReplyOpsFlowControl().permits()<10000){
+                    System.out.println("done="+ i +". available connection= "+ client.getConnector().getNoReplyOpsFlowControl().permits());
+                    Thread.sleep(1000);
+                }
+            }
+            
+            long endTime = System.currentTimeMillis();
+     
+            
+            System.out.println("It tooks "+ ((endTime-startTime))+"ms.");
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            client.shutdown();
         }
         
-        long endTime = System.currentTimeMillis();
- 
         
-        System.out.println("It tooks "+ ((endTime-startTime))+"ms.");
-        
-        client.shutdown();
+       
     }
 
 }
