1a1f31facb3f7d2b8795b188bdc595cb558399cf
jack <jack@jack-VirtualBox>
2015-07-06 20:16:20 +0900

    add speedtest

--- src/main/java/jackprivate/kttest/SpeedTest.java
+++ src/main/java/jackprivate/kttest/SpeedTest.java
@@ -8,13 +8,12 @@ import net.rubyeye.xmemcached.XMemcachedClientBuilder;
 import net.rubyeye.xmemcached.exception.MemcachedException;
 import net.rubyeye.xmemcached.utils.AddrUtil;
 
-public class QpsTest {
+public class SpeedTest {
 
     public static void main(String[] args) throws IOException {
         String target = args[0];
         String dataname = args[1];
-        int qps = Integer.valueOf(args[2]);
-        int second = Integer.valueOf(args[3]);
+        int count = Integer.valueOf(args[2]);
         XMemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(target));
         XMemcachedClient client=(XMemcachedClient) builder.build();
         
@@ -25,12 +24,8 @@ public class QpsTest {
             }
             String message = sb.toString();
             long startTime = System.currentTimeMillis();
-            for(long i=0;i<second;i++){
-                for(int j=0;j<qps;j++){
-                    client.setWithNoReply(dataname+"_"+i+"_"+j, 0, message+"_"+i+"_"+j);
-                }
-                System.out.println("done="+ i*qps +". available connection= "+ client.getConnector().getNoReplyOpsFlowControl().permits());
-                Thread.sleep(1000);
+            for(long i=0;i<count;i++){
+                client.set(dataname+"_"+i, 0, message+"_"+i);
             }
             
             long endTime = System.currentTimeMillis();
